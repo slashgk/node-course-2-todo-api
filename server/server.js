0c4f3714483282a9750bba93967c8d88c4bede78
GAURAV KUMAR <gaurav.grd@gmail.com>
2019-01-03 16:05:44 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyPraser = require('body-parser'); 
 
@@ -27,6 +29,19 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){ 
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
